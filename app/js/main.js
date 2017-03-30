6b8af984128e44941d926fc6bf0602da2f93f780
Vladimir Dmitriev <defurator@mail.ru>
2017-03-30 12:39:37 +0200

    init

---
+++ app/js/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+	/*$('#how_slider').bxSlider({
+	  auto: true,
+	  pager: false,
+	  minSlides: 4,
+	  maxSlides: 4,
+	  moveSlides: 1,
+	  slideWidth: 253,
+	  slideMargin: 28
+	});
+
+	$('.map-info_close').click(function(){
+		$('.map-info').hide();
+	});
+
+	$('.to-up').click(function() {
+    $('html, body').animate({ scrollTop: 0 }, 200);
+    return false;
+  });
+  if($('.to-up').length>0){
+    var waypoint_main1_1 = new Waypoint({
+      element: document.getElementById('to-up'),
+      handler: function(dir) {
+        if (dir === 'down') {
+          $('.to-up').addClass('to-up__on');
+        } else{
+          $('.to-up').removeClass('to-up__on');
+        }
+      },
+      offset: 0
+    })
+	}
+
+  $(".sitebutton").colorbox({inline:true, href:"#popup1"});
+  $(".trainers_link").colorbox({inline:true, href:"#popup1"});
+  $(".services_link").colorbox({inline:true, href:"#popup1"});
+  $(".price_link").colorbox({inline:true, href:"#popup1"});
+  $(".sitebanner_link").colorbox({inline:true, href:"#popup1"});
+  $(".rewards_item").colorbox({
+    rel:"group1",
+    current: "{current} из {total}"
+  });*/
+
+});
