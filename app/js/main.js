6927facdbe745ee2a307540da9b717f87061b760
Vladimir Dmitriev <defurator@mail.ru>
2017-04-05 22:43:37 +0200

    front-mobile, news

--- app/js/main.js
+++ app/js/main.js
@@ -1,45 +1,38 @@
 $(document).ready(function(){
 
-	/*$('#how_slider').bxSlider({
-	  auto: true,
-	  pager: false,
-	  minSlides: 4,
-	  maxSlides: 4,
-	  moveSlides: 1,
-	  slideWidth: 253,
-	  slideMargin: 28
-	});
-
-	$('.map-info_close').click(function(){
-		$('.map-info').hide();
-	});
-
-	$('.to-up').click(function() {
-    $('html, body').animate({ scrollTop: 0 }, 200);
-    return false;
+  //menu toggle
+  $('.menu-toggle__left').click(function(){
+    if ($(this).hasClass('menu-toggle__on')) {
+      $(this).removeClass('menu-toggle__on');
+      $(".menutop").removeClass('menutop__on');
+      $(".overlay").removeClass('overlay__on');
+    } else{
+      $(this).addClass('menu-toggle__on');
+      $(".menutop").addClass('menutop__on');
+      $(".overlay").addClass('overlay__on');
+      $(".menuleft").removeClass('menuleft__on');
+      $('.menu-toggle__right').removeClass('menu-toggle__on');
+    }
   });
-  if($('.to-up').length>0){
-    var waypoint_main1_1 = new Waypoint({
-      element: document.getElementById('to-up'),
-      handler: function(dir) {
-        if (dir === 'down') {
-          $('.to-up').addClass('to-up__on');
-        } else{
-          $('.to-up').removeClass('to-up__on');
-        }
-      },
-      offset: 0
-    })
-	}
-
-  $(".sitebutton").colorbox({inline:true, href:"#popup1"});
-  $(".trainers_link").colorbox({inline:true, href:"#popup1"});
-  $(".services_link").colorbox({inline:true, href:"#popup1"});
-  $(".price_link").colorbox({inline:true, href:"#popup1"});
-  $(".sitebanner_link").colorbox({inline:true, href:"#popup1"});
-  $(".rewards_item").colorbox({
-    rel:"group1",
-    current: "{current} из {total}"
-  });*/
+  $('.menu-toggle__right').click(function(){
+    if ($(this).hasClass('menu-toggle__on')) {
+      $(this).removeClass('menu-toggle__on');
+      $(".menuleft").removeClass('menuleft__on');
+      $(".overlay").removeClass('overlay__on');
+    } else{
+      $(this).addClass('menu-toggle__on');
+      $(".menuleft").addClass('menuleft__on');
+      $(".overlay").addClass('overlay__on');
+      $(".menutop").removeClass('menutop__on');
+      $('.menu-toggle__left').removeClass('menu-toggle__on');
+    }
+  });
+  //hide menu
+  $(".overlay").click(function(e) {
+    $(this).removeClass('overlay__on');
+    $('.menu-toggle').removeClass('menu-toggle__on');
+    $(".menutop").removeClass('menutop__on');
+    $(".menuleft").removeClass('menuleft__on');
+  });
 
 });
